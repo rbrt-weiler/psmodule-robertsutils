430921cb63900be223885548a2b27e1344fdb8c5
Robert WEILER <rbrt-weiler@mailbox.org>
2022-01-17 14:49:13 +0100

    Add Update-GitRepository.

---
+++ RobertsUtils/RobertsUtils.psm1
@@ -0,0 +1,31 @@
+function Update-GitRepository {
+    Param(
+        [String]$Directory
+    )
+
+    $BaseDirectory=$(Get-Location)
+    $GitCommand=$((Get-Command git).Source)
+
+    Write-Output "Info: Entering <$Directory>:"
+    Set-Location -Path "$Directory"
+    if (Test-Path -Path ".git") {
+        & "$GitCommand" "pull" "origin"
+        if (Test-Path -Path ".gitmodules") {
+            & "$GitCommand" "submodule" "update" "--remote" "--merge"
+        }
+        $Remotes = & "$GitCommand" "remote" "-v" | Select-String -Pattern '(fetch)' | Select-String -NotMatch -Pattern '^origin'
+        if ($Remotes) {
+            $Remotes.ToString() | ForEach-Object {
+                $RemoteName = ($_ | Select-String -Pattern '^(?<name>\w+)\s+').Matches[0].Groups['name'].Value
+                & "$GitCommand" "fetch" "$RemoteName"
+            }
+        }
+    } else {
+        Write-Output "Info: No <.git> found, skipping."
+    }
+    Write-Output ""
+
+    Set-Location -Path "$BaseDirectory"
+}
+
+Export-ModuleMember -Function Update-GitRepository
