22711f2543a2f8def576dcd5e5b5160c48abe3f0
Robert WEILER <rbrt-weiler@mailbox.org>
2022-01-23 08:30:56 +0100

    Add function Test-GitPath.

--- RobertsUtils/RobertsUtils.psm1
+++ RobertsUtils/RobertsUtils.psm1
@@ -1,3 +1,73 @@
+class GitPathStatus {
+    [String]$Path
+    [Bool]$PathExists
+    [Bool]$IsRepository
+    [Bool]$ContainsSubmodules
+    GitPathStatus() {
+        $this.Path = ""
+        $this.PathExists = $false
+        $this.IsRepository = $false
+        $this.ContainsSubmodules = $false
+    }
+}
+
+<#
+    .SYNOPSIS
+    Tests is a given path is a Git repository and if it contains submodules.
+
+    .DESCRIPTION
+    For a given path, this function will check if it is a Git repository. If it is, it will also check if the Git repository contains submodules.
+
+    .PARAMETER Name
+    The path that shall be tested.
+
+    .PARAMETER FullName
+    The path that shall be tested in absolute form; provided to work with Get-ChildItem.
+
+    .INPUTS
+    System.String[]
+
+    .OUTPUTS
+    GitPathStatus[]
+
+    .NOTES
+    The check performed by this function is solely based on the fact if a subdirectory ".git" (or ".gitmodules" for the submodule check) exists in the path provided by either Name or FullName.
+#>
+function Test-GitPath {
+    Param (
+        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [String]$Name,
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName = $true)]
+        [String]$FullName
+    )
+    begin {
+        $StatusCollection = @()
+    }
+    process {
+        $Status = [GitPathStatus]::new()
+        $TestPath = ""
+        if ($FullName) {
+            $TestPath = $FullName
+        } else {
+            $TestPath = $Name
+        }
+        $Status.Path = $TestPath
+        if (Test-Path -Path "$TestPath") {
+            $Status.PathExists = $true
+            if (Test-Path -Path (Join-Path -Path "$TestPath" -ChildPath ".git")) {
+                $Status.IsRepository = $true
+                if (Test-Path -Path (Join-Path -Path "$TestPath" -ChildPath ".gitmodules")) {
+                    $Status.ContainsSubmodules = $true
+                }
+            }
+        }
+        $StatusCollection += $Status
+    }
+    end {
+        return $StatusCollection
+    }
+}
+
 <#
     .SYNOPSIS
     Updates a Git repository.
@@ -101,5 +171,6 @@ function Update-GitRepositories {
     }
 }
 
+Export-ModuleMember -Function Test-GitPath
 Export-ModuleMember -Function Update-GitRepository
 Export-ModuleMember -Function Update-GitRepositories
